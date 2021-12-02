d3fd82b98d4928248a54720e4277df37402da862
Dan Horák <dan@danny.cz>
2021-12-02 10:50:25 +0100

    fix ppc/ppc64 detection for GCC

--- pffft.c
+++ pffft.c
@@ -96,7 +96,7 @@
 /*
    Altivec support macros 
 */
-#if !defined(PFFFT_SIMD_DISABLE) && (defined(__ppc__) || defined(__ppc64__))
+#if !defined(PFFFT_SIMD_DISABLE) && (defined(__ppc__) || defined(__ppc64__) || defined(__powerpc__) || defined(__powerpc64__))
 #include <altivec.h>
 typedef vector float v4sf;
 #  define SIMD_SZ 4
